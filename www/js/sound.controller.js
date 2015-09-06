4cab2d771ddf4d6128bdb9818a170f63143d4be1
ashteya <ashteya@gmail.com>
2015-09-06 12:10:54 +0100

    Initial version

---
+++ www/js/sound.controller.js
@@ -0,0 +1,34 @@
+(function() {
+	angular.module('starter').controller('SoundController', ['$ionicPlatform', '$timeout',  '$cordovaNativeAudio', SoundController]);
+	
+	function SoundController($ionicPlatform, $timeout, $cordovaNativeAudio) {
+		var vm = this;
+		
+		$ionicPlatform.ready(function() {
+			$cordovaNativeAudio.preloadSimple('snare', 'audio/snare.mp3')
+							   .then(function(msg) { console.log(msg); })
+							   .catch(function(error) { console.error(error); });
+							   
+			$cordovaNativeAudio.preloadSimple('hi-hat', 'audio/highhat.mp3')
+							   .then(function(msg) { console.log(msg); })
+							   .catch(function(error) { console.error(error); });
+							   
+			$cordovaNativeAudio.preloadSimple('bass', 'audio/bass.mp3')
+							   .then(function(msg) { console.log(msg); })
+							   .catch(function(error) { console.error(error); });
+							   
+			$cordovaNativeAudio.preloadSimple('bongo', 'audio/bongo.mp3')
+							   .then(function(msg) { console.log(msg); })
+							   .catch(function(error) { console.error(error); });
+		});
+		
+		vm.play = function(sound) {
+			$cordovaNativeAudio.play(sound)
+							   .then(function(msg) { console.log(msg); })
+							   .catch(function(error) { console.error(error); });
+		};
+		
+		return vm;
+	}
+})();
+
